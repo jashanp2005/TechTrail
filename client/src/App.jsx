a186b03f9bff62c22e85ca854e7fed969b39254d
Jashanpreet Singh <jashan.2005preetsingh@gmail.com>
2024-05-24 08:17:42 +0530

    react-router-dom setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
 
 function App() {
   return (
-    <div className='text-3xl text-red-800'>
-      App
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element={<Home/>}></Route>
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
