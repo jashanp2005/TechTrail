258552de2b7f699e6c6619a31c092c6dd6cc5250
Jashanpreet Singh <160238258+jashanp2005@users.noreply.github.com>
2024-09-21 12:40:07 +0000

    done

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Button } from 'flowbite-react'
+import { Button } from 'flowbite-react';
 import { AiFillGoogleCircle } from 'react-icons/ai';
 import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
 import { app } from '../firebase';
@@ -7,38 +6,33 @@ import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
 
-
-function OAuth() {
-
-    const auth = getAuth(app);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    const handleGoogleClick = async () => {
-        const provider = GoogleAuthProvider();
-        provider.setCustomParameters({prompt: 'select_account'});
+export default function OAuth() {
+    const auth = getAuth(app)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const handleGoogleClick = async () =>{
+        const provider = new GoogleAuthProvider()
+        provider.setCustomParameters({ prompt: 'select_account' })
         try {
-            const resultsFormGoogle = await signInWithPopup(auth, provider)
+            const resultsFromGoogle = await signInWithPopup(auth, provider)
             const res = await fetch('/api/auth/google', {
                 method: 'POST',
-                headers: {'Content-Type': 'application/json'},
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    name: resultsFormGoogle.user.displayName,
-                    email: resultsFormGoogle.user.email,
-                    googlePhotoUrl: resultsFormGoogle.user.photoURL
+                    name: resultsFromGoogle.user.displayName,
+                    email: resultsFromGoogle.user.email,
+                    googlePhotoUrl: resultsFromGoogle.user.photoURL,
+                }),
                 })
-            })
-            const data = await res.json();
-            if(res.ok){
-                dispatch(signInSuccess(data));
-                navigate('/');
+            const data = await res.json()
+            if (res.ok){
+                dispatch(signInSuccess(data))
+                navigate('/')
             }
-        } 
-        catch (error) {
+        } catch (error) {
             console.log(error);
         }
-    }
-    
+    } 
   return (
     <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
         <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
@@ -46,5 +40,3 @@ function OAuth() {
     </Button>
   )
 }
-
-export default OAuth;
